70ca8ae5f70f3319989ef00d0fe1da601fea6270
E-greene <phoex1106@gmail.com>
2022-11-12 23:04:51 +0900

    구체클래스에 의존중DIP,OCP 위반 문제 해결 전

--- core/src/main/java/hello1/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello1/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello1.core.order;
 
 import hello1.core.discount.DiscountPolicy;
 import hello1.core.discount.FixDiscountPolicy;
+import hello1.core.discount.RateDiscountPolicy;
 import hello1.core.member.Member;
 import hello1.core.member.MemberRepository;
 import hello1.core.member.MemoryMemberRepository;
@@ -9,7 +10,9 @@ import hello1.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
