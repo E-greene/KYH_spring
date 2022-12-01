0f6bac2a163604dbc3f6caa9f75e27323e8a3ce9
E-greene <phoex1106@gmail.com>
2022-12-01 21:33:13 +0900

    HTTP요청메시지 JSON, HTTP응답HTTP API 예제 추가

---
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello").addObject("data","hello!");
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello!");
+        return "response/hello";
+    }
+
+    //경로의 이름이랑 return할 뷰의 논리적 이름이 똑같으면 return 없어도됨
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!");
+    }
+}
