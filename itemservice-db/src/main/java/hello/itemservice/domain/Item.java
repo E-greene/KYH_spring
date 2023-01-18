d2252673d8d979cf3b56c9a735e23fceb63814eb
E-greene <phoex1106@gmail.com>
2023-01-18 23:03:33 +0900

    JPA란 , V1적용

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,22 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Data
+@Entity // JPA가 사용하는 객체라는 뜻
+@Table(name = "item")  //객체 명이랑 같으면 생략가능
 public class Item {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //@Id 는 테이블의 PK와 해당 필드 매핑
     private Long id;
 
+    @Column(name = "item_name", length = 10) // 이름 같으면 생략 가능 price, quantity
     private String itemName;
     private Integer price;
     private Integer quantity;
