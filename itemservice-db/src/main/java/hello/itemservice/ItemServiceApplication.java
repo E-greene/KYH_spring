5c22b9fb9b05ec86b0ea1057bccc6c5205c2495d
E-greene <phoex1106@gmail.com>
2023-01-20 05:27:55 +0900

    querydsl

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -15,7 +15,8 @@ import javax.sql.DataSource;
 @Slf4j
 //@Import(MemoryConfig.class)
 //@Import(JpaConfig.class)
-@Import(SpringDataJpaConfig.class)
+//@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
